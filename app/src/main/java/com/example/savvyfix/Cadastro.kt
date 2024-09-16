8edb97863e7bb31e67015c06f37423090ede656c
douglas <douglas.arau@hotmail.com>
2024-09-16 16:13:14 -0300

    Fim da navegabilidade da pagina cadastro

--- app/src/main/java/com/example/savvyfix/Cadastro.kt
+++ app/src/main/java/com/example/savvyfix/Cadastro.kt
@@ -1,20 +1,36 @@
 package com.example.savvyfix
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnCadastro: Button = findViewById(R.id.btnCadastro)
+        val entreAquiCadastro : TextView = findViewById(R.id.entreAquiCadastro)
+
+        btnCadastro.setOnClickListener {
+            IrParaHome()
         }
+
+        entreAquiCadastro.setOnClickListener{
+            IrParaLogin()
+        }
+    }
+    private fun IrParaHome(){
+        val home = Intent(this, MainActivity::class.java)
+        startActivity(home)
+    }
+
+    private fun IrParaLogin() {
+        val login = Intent(this, Login::class.java)
+        startActivity(login)
     }
 }
